da549cdfaa8eddc8bc348186430f6b75b6264eb8
madina_afrolicous <30729081+madinafrolicious@users.noreply.github.com>
2022-03-01 21:08:45 +0100

    implementation footer sur mobile

--- js/burger.js
+++ js/burger.js
@@ -3,6 +3,7 @@ let sidebar = document.querySelector('.sidebar')
 let navigation = document.querySelector('#nav-list');
 let navbar = document.querySelector('#navbar');
 let socialHeader = document.querySelector('#social-header');
+let footer = document.querySelector('#footer');
 
 burger.addEventListener('click', function(){
    burger.classList.toggle('active-burger');
@@ -10,4 +11,5 @@ burger.addEventListener('click', function(){
    sidebar.classList.toggle('active-sidebar');
    navbar.classList.toggle('active');
   socialHeader.classList.toggle('active');
+  footer.classList.toggle('delete');
  })
--- js/filters-portfolio.js
+++ js/filters-portfolio.js
@@ -1,3 +1,43 @@
+// let displayPage = function(a){
+//     var li = a.parentNode;
+//     // console.log(li)
+//     var div =a.parentNode.parentNode.parentNode.parentNode.parentNode;
+//     console.log(div)
+  
+// //verifier si la classe n'est pas déja active sur le lien cliqué et je ne fais rien si c le cas
+// if(li.classList.contains('active')){
+//     return false;
+// };
+  
+//    //retirer la class active des elements tabs (li) de la div conteneur
+//     div.querySelector('.nav-btns .active').classList.remove('active');
+
+//     //j'ajoute la classe active au li cliqué
+//     li.classList.add('active');
+
+// //retirer la class active des elements contenu de la div conteneur
+// div.querySelector('.page-content.active').classList.remove('active');
+
+
+//     //j'ajoute la class active des elements contenu de la div conteneur qui
+//     div.querySelector(a.getAttribute('href')).classList.add('active');
+// }
+
+// let navBtns = document.querySelectorAll('.nav-btns a');
+// for(let i=0; i< navBtns.length; i++){
+//     navBtns[i].addEventListener('click', function(e){
+      
+//         displayPage(this);
+//         console.log("clicked")
+//     })
+
+// };
+
+// var hash = window.location.hash;
+// var a = document.querySelector('a[href="'+hash+'"]');
+// if (a !==null && a.parentNode.classList.contains('active')){
+// displayPage(a);
+// }
 let filters = document.querySelectorAll('.filter');
 let projects = document.querySelectorAll('.portfolio-item');
 
