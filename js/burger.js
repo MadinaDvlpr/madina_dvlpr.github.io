437a5410ca4052642c34167e9aab4a38854c1dbe
madina_afrolicous <30729081+madinafrolicious@users.noreply.github.com>
2022-03-01 16:10:48 +0100

    alignement social icons

--- js/burger.js
+++ js/burger.js
@@ -2,7 +2,7 @@ let burger = document.querySelector('#icon-burger');
 let sidebar = document.querySelector('.sidebar')
 let navigation = document.querySelector('#nav-list');
 let navbar = document.querySelector('#navbar');
-let socialHeader = document.querySelector('#social-header')
+let socialHeader = document.querySelector('#social-header');
 
 burger.addEventListener('click', function(){
    burger.classList.toggle('active-burger');
