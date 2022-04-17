ed232b5ff2d73dbda311a5430377ba5546900fb2
magda_dev <30729081+Magda-dev@users.noreply.github.com>
2022-04-17 08:56:00 +0200

    maj todo

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,7 +1,11 @@
+
 const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
 const deleteListButton = document.querySelector('[data-delete-list-btn]')
+const editTaskButton = document.querySelector('[data-edit-tasks-btn]')
+const deleteTaskButton = document.querySelector('[data-delete-tasks-btn]')
+console.log(deleteTaskButton)
 const listDisplayContainer = document.querySelector('[data-list-display-container]')
 const listTitleElement = document.querySelector('[data-list-title]')
 const listCountElement = document.querySelector('[data-list-count]')
@@ -10,11 +14,15 @@ const taskTemplate = document.getElementById('task-template')
 const newTaskForm = document.querySelector('[data-new-task-form]')
 const newTaskInput = document.querySelector('[data-new-task-input]')
 const clearCompleteTasksButton = document.querySelector('[data-clear-complete-tasks-btn]')
-
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
-var lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
-var selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+
+
+
+
 
 listsContainer.addEventListener('click', e => {
   if (e.target.tagName.toLowerCase() === 'li') {
@@ -39,6 +47,13 @@ clearCompleteTasksButton.addEventListener('click', e => {
   saveAndRender()
 })
 
+// deleteTaskButton.addEventListener('click', e => {
+//   const selectedList = lists.find(list => list.id === selectedListId)
+//   selectedList.tasks = selectedList.tasks.filter(task => !task.complete)
+//   saveAndRender()
+// })
+
+
 deleteListButton.addEventListener('click', e => {
   lists = lists.filter(list => list.id !== selectedListId)
   selectedListId = null
@@ -46,25 +61,20 @@ deleteListButton.addEventListener('click', e => {
 })
 
 newListForm.addEventListener('submit', e => {
-     //j'annule le rafraichissement dela page quand je tape entrer, car par defaut la page se rafraichie quand on press entrer
   e.preventDefault()
-     //je recupere le nom de la liste
   const listName = newListInput.value
   if (listName == null || listName === '') return
   const list = createList(listName)
-      //je nettoie le champ une fois le bouton submit appuyé
   newListInput.value = null
-  //j'envoie le mot tapé dans le tableau des listes de taches
   lists.push(list)
-  
-    //j'affiche le nouvelle list grace à la fonction render
   saveAndRender()
 })
 
 newTaskForm.addEventListener('submit', e => {
   e.preventDefault()
   const taskName = newTaskInput.value
-  if (taskName == null || taskName === '') return
+  if (taskName == 
+  null || taskName === '') return
   const task = createTask(taskName)
   newTaskInput.value = null
   const selectedList = lists.find(list => list.id === selectedListId)
@@ -73,8 +83,6 @@ newTaskForm.addEventListener('submit', e => {
 })
 
 
-
-//je crée une fonction de création de list qui nous retouorne un objet list dont l'id sera la date de saisie en string, le nom sera le nom saisi et qui aura un tableau de taches pour le moment vide
 function createList(name) {
   return { id: Date.now().toString(), name: name, tasks: [] }
 }
@@ -88,7 +96,7 @@ function saveAndRender() {
   render()
 }
 
-//Fonction pour enregistrer nos listes dans le local storage
+
 function save() {
   localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
   localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
@@ -99,7 +107,7 @@ function render() {
   renderLists()
 
   const selectedList = lists.find(list => list.id === selectedListId)
-      //j'affiche les taches quand la liste est selectionnée et je cache quand la liste n'est pas selectionnée
+console.log(selectedList)
 
   if (selectedListId == null) {
     listDisplayContainer.style.display = 'none'
@@ -113,12 +121,10 @@ function render() {
 }
 
 function renderTasks(selectedList) {
-       // importNode permet de cloner le template des taches
-    //.content va target la div task et en ajoutant True ça va recupérer toutes les div à l'intérieur
-    //si on met pas true on aura que la div task vide
+     
   selectedList.tasks.forEach(task => {
     const taskElement = document.importNode(taskTemplate.content, true)
-    const checkbox = taskElement.querySelector('input')
+    const checkbox = taskElement.querySelector('#checkbox')
     checkbox.id = task.id
     checkbox.checked = task.complete
     const label = taskElement.querySelector('label')
@@ -136,17 +142,17 @@ function renderTaskCount(selectedList) {
 
 function renderLists() {
   lists.forEach(list => {
-      //je crée un élément list
+     
     const listElement = document.createElement('li')
     listElement.dataset.listId = list.id
-    //j'applique la mise en forme css à cet élément 
+   
     listElement.classList.add("list-name")
-         //je récupère la saisie 
+     
     listElement.innerText = list.name
     if (list.id === selectedListId) {
       listElement.classList.add('active-list')
     }
-    //j'ajoute l'élément au conteneur
+    
     listsContainer.appendChild(listElement)
   })
 }
