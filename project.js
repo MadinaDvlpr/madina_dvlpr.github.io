bc972843ca0e007c96fc3d52b5c7bb5ebeea7ff2
magda_dev <30729081+Magda-dev@users.noreply.github.com>
2023-09-25 09:08:23 +0200

    UPDATE Branding

---
+++ project.js
@@ -0,0 +1,62 @@
+let projects = [
+    {
+        name: "project one",
+        tags: "#javascript, #fullstack, #ui/ux, #backend",
+        image: "projet1.png",
+    },
+    {
+        name: "project two",
+        tags: "#javascript, #fullstack",
+        image: "projet2.png",
+    },
+    {
+        name: "project three",
+        tags: "#javascript, #fullstack",
+        image: "projet3.png",
+    },
+    {
+        name: "project four",
+        tags: "#javascript, #fullstack",
+        image: "projet1.png",
+    },
+    {
+        name: "project five",
+        tags: "#javascript, #fullstack",
+        image: "projet2.png",
+    },
+    {
+        name: "project six",
+        tags: "#javascript, #fullstack",
+        image: "projet3.png",
+    },
+    {
+        name: "project seven",
+        tags: "#javascript, #fullstack",
+        image: "projet2.png",
+    },
+    {
+        name: "project eight",
+        tags: "#Ui/Ux",
+        image: "projet1.png",
+    },
+    {
+        name: "project nine",
+        tags: "#javascript, #fullstack",
+        image: "projet3.png",
+    },
+    {
+        name: "project ten",
+        tags: "#backend",
+        image: "projet1.png",
+    },
+    {
+        name: "project eleven",
+        tags: "#backend",
+        image: "projet1.png",
+    },
+    {
+        name: "project twelve",
+        tags: "#fullstack",
+        image: "projet1.png",
+    }
+]
